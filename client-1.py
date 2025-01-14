73d9c6fc428a28ad78b8de7a49b7fdd3659f449b
TaljaaEb <138305368+TaljaaEb@users.noreply.github.com>
2025-01-14 05:42:49 +0000

    Update client-1.py

--- client-1.py
+++ client-1.py
@@ -16,29 +16,16 @@ def get_username():
     USER = os.getlogin()
     return USER
 
-#ip = get_ip()
-ip = 'the-ip-where-auth.py-is-running'
+ip = get_ip()
+IP = 'the-ip-where-auth.py-is-running'
 port = 443
 user = get_username()
 
 public_key, private_key = rsa.newkeys(1024)
 public_partner = None
 
-
-#choice = input("Do you want to host (1) or to connect (2): ")
-
-#if choice == "1":
-#    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#    server.bind((ip, port))
-#    server.listen()
-#
-#    client, addr = server.accept()
-#    client.send(public_key.save_pkcs1("PEM"))
-#    public_partner = rsa.PublicKey.loadpkcs1(client.recv(1024))
-#   
-#elif choice == "2":
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((ip, port))
+client.connect((IP, port))
 public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
 client.send(public_key.save_pkcs1("PEM"))
 
@@ -47,8 +34,7 @@ client.send(public_key.save_pkcs1("PEM"))
 
 def sending_message(c, public_partner):
     while True:
-        #message = ip +  user
-        message = input("rc : ")
+        message = ip +  user
         c.send(rsa.encrypt(message.encode(), public_partner))
        
 def receiving_message(c, private_key):
@@ -58,4 +44,3 @@ def receiving_message(c, private_key):
         
 threading.Thread(target=sending_message, args=(client, public_partner)).start()
 threading.Thread(target=receiving_message, args=(client, private_key)).start()
-
